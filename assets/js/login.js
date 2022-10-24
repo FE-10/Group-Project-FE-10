e9d062b28b3d2efce3d8b5e2efa921676e2f212f
Muhammad Sadewo Wicaksono <114298461+musadewo@users.noreply.github.com>
2022-10-24 23:25:23 +0700

    Fourth Commit

--- assets/js/login.js
+++ assets/js/login.js
@@ -23,7 +23,8 @@ async function GetLogin(email, password) {
     localStorage.setItem("Email", user.email);
     localStorage.setItem("Username", user.username);
     console.log("login sukses");
-    window.location.href = "dashboard.html";
+    alert("Berhasil Login");
+    window.location.href = "home.html";
     // let ambilID = localStorage.getItem("ID");
     // let ambildata = JSON.parse(ambilID)
     // console.log(ambildata)
